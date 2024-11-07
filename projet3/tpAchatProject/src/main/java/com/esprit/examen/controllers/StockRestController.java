5a2f407d52412dc371a7b3cc75acddec734e6b97
Nay <80353519+nyrouz911@users.noreply.github.com>
2024-11-07 01:42:09 +0100

    Update StockRestController.java

--- projet3/tpAchatProject/src/main/java/com/esprit/examen/controllers/StockRestController.java
+++ projet3/tpAchatProject/src/main/java/com/esprit/examen/controllers/StockRestController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.*;
 import com.esprit.examen.entities.Stock;
 import com.esprit.examen.services.IStockService;
 
-import io.swagger.annotations.Api;
+
 
 @RestController
-@Api(tags = "Gestion des stocks")
+
 @RequestMapping("/stock")
 @CrossOrigin("*")
 public class StockRestController {
